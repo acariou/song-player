f8a3c73fcdbe1c989d6e07ff4294fdcb42ab566c
CARIOU <arnaud.cariou@openclassrooms.com>
2014-06-13 00:27:07 +0200

    Installation de compass et lancement du projet

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+'use strict';
+// Récupération du namespace
+var songPlayer = (function($) {
+
+    function MyPlayer (el) {
+        
+        this.el = el;
+        
+        this.init = function () {
+            console.log(this.el);
+        };
+        
+        return this.init();
+    };
+    
+    return {
+        MyPlayer: MyPlayer
+    };
+    
+})(jQuery);
