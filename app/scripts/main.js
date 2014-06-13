b2ce6a1ded771231b245d5d70ccde7ad77f6bdf5
CARIOU <arnaud.cariou@openclassrooms.com>
2014-06-13 10:26:12 +0200

    Avancement

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,17 +1,29 @@
 'use strict';
-// Récupération du namespace
+/** Ici on utilise pas un objet mais une closure déclaré à l'aide d'une 
+  * variable afin d'émuler une notion de module
+  */
 var songPlayer = (function($) {
 
+    /**
+      * Constructeur de l'objet player
+      */
     function MyPlayer (el) {
         
         this.el = el;
+        this.$el = $(el);
         
         this.init = function () {
-            console.log(this.el);
+            console.log(this.$el);
+            this._create();
+        };
+        this._create = function () {
+            var player = document.querySelectorAll(this.el);
+            console.log(player);
+            player.pause();
         };
         
         return this.init();
-    };
+    }
     
     return {
         MyPlayer: MyPlayer
