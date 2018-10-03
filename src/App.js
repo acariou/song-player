bf7945a2086c5055474cb7becbf9cdddcdfd3e39
acariou <contact@neolame.com>
2018-10-03 18:43:17 +0200

    Eject

--- src/App.js
+++ src/App.js
@@ -10,44 +10,6 @@ const track = {
     name: 'tralala'
   }
 };
-//    _setText = function (el, text) {
-//        el.text(text);
-//    };
-
-// $scope.player = function() {if ( audioTrack.paused ) {playButton.removeClass('fa-play').addClass('fa-pause');audioTrack.play();} else {playButton.removeClass('fa-pause').addClass('fa-play');audioTrack.pause();}};
-// Fonction de d'activation/desactivation du volume
-// $scope.muter = function () {
-//     if ( audioTrack.volume === 0 ) {
-//         audioTrack.volume = restorValue;
-//         volumeSlider.value = restorValue;
-//     } else {
-//         restorValue = volumeSlider.value;
-//         audioTrack.volume = 0;
-//         volumeSlider.value = 0;
-//     }
-// };
-
-// $scope.volumizer = function () {
-//     if ( audioTrack.volume === 0 ) {
-//         _setText(muteButton, 'Unmute');
-//     } else {
-//         _setText(muteButton, 'Mute');
-//     }
-// };
-
-// $scope.finish = function () {
-//     audioTrack.currentTime = 0;
-//     _setText(playButton, 'Play');
-// };
-
-// $scope.updatePlayhead = function () {
-//     playhead.value = audioTrack.currentTime;
-//     var s = parseInt(audioTrack.currentTime % 60);
-//     var m = parseInt((audioTrack.currentTime / 60) % 60);
-//     s = (s >= 10) ? s : '0' + s;
-//     m = (m >= 10) ? m : '0' + m;
-//     playbacktime.innerHTML = m + ':' + s ;
-// };
 
 //songPlayer.prototype = {
 // self.volumeSlider.addEventListener('input', function(){self.audioTrack.volume = self.volumeSlider.value;}, false);
@@ -58,6 +20,52 @@ const track = {
 //audioTrack.removeAttribute('controls');},
 
 class App extends Component {
+  player() {
+    if ( audioTrack.paused ) {
+      playButton.removeClass('fa-play').addClass('fa-pause');
+      audioTrack.play();
+    } else {
+      playButton.removeClass('fa-pause').addClass('fa-play');
+      audioTrack.pause();
+    }
+  };
+  // Fonction de d'activation/desactivation du volume
+  muter() {
+    if ( audioTrack.volume === 0 ) {
+      audioTrack.volume = restorValue;
+      volumeSlider.value = restorValue;
+    } else {
+      restorValue = volumeSlider.value;
+      audioTrack.volume = 0;
+      volumeSlider.value = 0;
+    }
+  };
+
+  volumizer() {
+    if ( audioTrack.volume === 0 ) {
+      this._setText(muteButton, 'Unmute');
+    } else {
+      this._setText(muteButton, 'Mute');
+    }
+  };
+
+  finish = function () {
+    audioTrack.currentTime = 0;
+    this._setText(playButton, 'Play');
+  };
+
+  _setText(el, text) {
+      el.text(text);
+  };
+
+  updatePlayhead() {
+     playhead.value = audioTrack.currentTime;
+     let s = parseInt(audioTrack.currentTime % 60);
+     let m = parseInt((audioTrack.currentTime / 60) % 60);
+     s = (s >= 10) ? s : '0' + s;
+     m = (m >= 10) ? m : '0' + m;
+     playbacktime.innerHTML = m + ':' + s ;
+  };
 
   render() {
     return (
@@ -73,10 +81,10 @@ class App extends Component {
                 <meta itemprop="duration" content="PT2M29S" />
                 <div id="fader">
                     <input type="range" min="0" max="1" step="any" value="1" id="volumeSlider" orient="vertical" />
-                    <button type="button" class="fa fa-volume-up" onClick="muter()" data-ctrl="btnVolume"></button>
+                    <button type="button" className="fa fa-volume-up" onClick={this.muter} data-ctrl="btnVolume"></button>
                 </div>
                 <div id="playback">
-                    <button type="button" onClick="player()" data-ctrl="play" class="fa fa-play"></button>
+                    <button type="button" onClick={this.player} data-ctrl="play" className="fa fa-play"></button>
                     <progress min="0" max="100" value="0" id="playhead"></progress>
                 </div>
                 <audio src="medias/01-rainbowblues.mp3" controls id="audiotrack" preload="auto" itemprop="audio"></audio>
