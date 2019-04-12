5191cc6e2d63bf70626284d5d0c8db26241bdad2
CARIOU <contact@neolame.com>
2019-04-12 14:21:30 +0200

    passage à react

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,11 +1,13 @@
 import React from 'react';
+import logo from './logo.png';
+import soundcloud from './soundcloud.png';
 
 class Footer extends React.Component {
   render(){
     return(
       <div className="footer">
-        <p>Love from <img src="./logo.png" className="logo" alt="" />
-          & <img src="./soundcloud.png" className="soundcloud" alt="" />
+        <p>Love from <img src={logo} className="logo" alt="" />
+          & <img src={soundcloud} className="soundcloud" alt="" />
         </p>
       </div>
     )
