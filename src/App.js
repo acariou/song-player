a985853f475f997dc138fb4062ec965b3d5e1a2f
Arnaud CARIOU <arnaud.cariou@ext.mpsa.com>
2020-05-10 18:54:23 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
 import React from 'react';
-import Search from './Components/Search';
-import Details from './Components/Details';
-import Player from './Components/Player';
-import Progress from './Components/Progress';
-import Footer from './Components/Footer/Footer';
+import Button from '@material-ui/core/Button';
+
 import './App.css';
+import 'typeface-roboto';
 
 function App() {
   return (
     <div>
-      <Search tracks={['']}/>
-      <Details title={'Track title'}/>
-      <Player/>
-      <Progress position={'0.3'} elapsed={'00:00'} total={'0:40'}/>
-      <Footer/>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-import { library } from '@fortawesome/fontawesome-svg-core';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-    faPlay,
-    faPause,
-    faBackward,
-    faStop,
-    faRandom,
-    faForward } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faPlay, faPause, faBackward, faStop, faRandom, faForward);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
